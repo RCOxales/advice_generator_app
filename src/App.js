894b3123dc5ad30d3a834eea7d837d0e84880624
RCOxales <jnay.oe14@gmail.com>
2022-07-03 01:25:24 -0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import desktopdivider from './Image/pattern-divider-desktop.svg';
+import diceicon from './Image/icon-dice.svg';
+import { useEffect, useState } from 'react';
+import Loading from './Image/loading';
 
 function App() {
+const [advice, setaAdvice] = useState();
+const[isloading, SetIsloading] = useState(true);
+
+const randomadvice = async()=>{
+  SetIsloading(true);
+  const response = await fetch('https://api.adviceslip.com/advice');
+  const advicedata = await response.json();
+
+  setaAdvice(advicedata);
+  SetIsloading(false);
+};
+useEffect(() => {
+randomadvice();
+}, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+    <div className='h-screen bg-DarkBlue flex items-center justify-center flex-col'>
+      {isloading ? (
+        <Loading />
+
+      ): (
+<>
+        <div className='w-[500px] rounded-lg h-fit bg-DarkGrayishBlue p-12'>
+        <h2 className='text-NeonGreen  text-sm text-center mb-5 font-semibold'>Advice #{advice?.slip.id}</h2>
+        <p className='text-LightCyan mb-10 font-extrabold'>
+          “{advice?.slip.advice}”
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={desktopdivider} alt='desktop divider' />
+      </div>
+      <button className='h-12 w-12  bg-NeonGreen -mt-5 rounded-full flex items-center justify-center' onClick={randomadvice}>
+        <img src= {diceicon} alt='dice' className='h-5 w-5'/>
+      </button>
+</>
+        )}
     </div>
+    </>
   );
 }
 
